a13463b0ea6d879767f499467ebf6d84ee893a62
jhovahn <nhavohj@gmail.com>
2026-01-22 20:28:16 -0800

    Add ability to dismiss alarm from success screen

--- app/src/main/java/com/example/habittracker/screens/SuccessScreen.kt
+++ app/src/main/java/com/example/habittracker/screens/SuccessScreen.kt
@@ -1,17 +1,14 @@
 package com.example.habittracker.screens
 
+import android.content.Intent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.offset
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
@@ -21,16 +18,18 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.example.habittracker.alarm.AlarmService
 import com.example.habittracker.viewmodel.HabitViewModel
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SuccessScreen(navController: NavController, id: Long, viewModel: HabitViewModel) {
     val habits by viewModel.habits.collectAsState()
     val habit = habits.find { it.id.toLong() == id }
+    val context = LocalContext.current
+    val serviceIntent = Intent(context, AlarmService::class.java)
     Scaffold { innerPadding ->
         Column(
             modifier = Modifier
@@ -43,7 +42,7 @@ fun SuccessScreen(navController: NavController, id: Long, viewModel: HabitViewMo
             Column(
                 verticalArrangement = Arrangement.spacedBy(16.dp),
 
-            ) {
+                ) {
                 val minutes = (habit?.timerDuration?.toInt() ?: 0) / 1000 / 60
                 val minutesString = if (minutes > 1) "mins" else "min"
                 Text(text = "✅ ${habit?.name}")
@@ -52,9 +51,12 @@ fun SuccessScreen(navController: NavController, id: Long, viewModel: HabitViewMo
             }
 
             IconButton(
-                modifier = Modifier.size(15.dp).offset(y=32.dp),
+                modifier = Modifier
+                    .size(15.dp)
+                    .offset(y = 32.dp),
                 onClick = {
                     navController.navigate("home") {}
+                    context.stopService(serviceIntent)
                 },
             ) {
                 Icon(
